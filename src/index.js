851321a5bf61ff2c8bb14ce775b077d501768390
thneves <tsneves11@gmail.com>
2021-04-02 14:37:10 -0300

    Inital home page inside index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-alert('It is working');
+import './styles.css'
+
+function component() {
+  const content = document.getElementById('content');
+  content.classList.add('home-content');
+
+  const title = document.createElement('h1');
+  title.classList.add('title');
+  title.innerHTML = 'The Tower'
+
+  const copy = document.createElement('h4');
+  copy.classList.add('copy');
+  copy.innerHTML = 'Unique experience, every single time'
+
+  content.appendChild(title);
+  content.appendChild(copy);
+
+  return content;
+}
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,18 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
